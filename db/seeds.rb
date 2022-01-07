f05e1c8bd1ca45b8ab22a93969b53bba8ad19906
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-01-07 15:36:55 -0800

    Faker updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ Product.destroy_all
 Review.destroy_all
 
 50.time do |index|
-  product = Product.create!({name: Faker::Superhero.random})
+  product = Product.create!({name: Faker::Superhero.random, origin: Faker::Address.city, cost: Faker::Number.number(digits: 2)})
   250.times do |index|
-    Review.create!({name: Faker::Name.name, product_id: product_id })
+    Review.create!({author: Faker::Name.name, content_body: Faker::Books::Dune.quote, product_id: product_id })
   end
 end
 
