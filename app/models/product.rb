0f37cd243e331aaaa7c6036f27c0081505f569c5
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-01-07 13:47:54 -0800

    working on validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
+  validates :cost, presence: true
+  validates :origin, presence:true
+  validates_length_of :origin, maximum: 50
   validates_length_of :name, maximum: 50
   before_save(:titleize_product)
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
   validates :name, presence: true
+  validates :rating, numericality: { in: 1..5 }
   validates_length_of :name, maximun: 50
   before_save(:titleize_review)
 
