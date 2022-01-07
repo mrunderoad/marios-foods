0db59188a044671e68fe721311a83993c9f946d7
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-01-07 09:25:39 -0800

    working on layouts

--- db/migrate/20220107164028_create_reviews.rb
+++ db/migrate/20220107164028_create_reviews.rb
@@ -1,6 +1,6 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
-    create_table :songs do |t|
+    create_table :reviews do |t|
       t.column(:author, :string)
       t.column(:content_body, :string)
       t.column(:rating, :integer)
